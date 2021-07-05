79b8653148ff7adf528b1ebbee6cd0f1794294ed
Markos Moura <manomarkokillar006@gmail.com>
2021-07-05 18:09:45 -0300

    telinha do projeto

---
+++ src/br/com/model/Login.java
@@ -0,0 +1,23 @@
+package br.com.model;
+
+public class Login {
+    private String usuario;
+    private int senha;
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public int getSenha() {
+        return senha;
+    }
+
+    public void setSenha(int senha) {
+        this.senha = senha;
+    }
+}
+
